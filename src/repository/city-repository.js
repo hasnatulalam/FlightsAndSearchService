b175b7db0d4a2da92d699931a6265d120bec9fae
hasnatulalam <hasnatualam056@gmail.com>
2023-10-17 09:21:42 +0600

    Add city Service

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -20,5 +20,26 @@ class CityRepository{
         throw{error}
     }
    }
+   async updateCity(cityId,{data}){
+    try {
+       const city =await City.update(data,{
+        where:{
+            id:cityId
+        }
+       }) 
+       return city;
+    } catch (error) {
+        throw{error} 
+    }
+
+   }
+   async getCity(cityId){
+      try {
+        const city =await City.findByPk(cityId)
+        return city;
+      } catch (error) {
+        throw{error}
+      }
+   }
 }
 module.exports=CityRepository;
